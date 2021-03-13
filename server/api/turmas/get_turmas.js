98f59fa27f6b1a0087e39c289b829028b7cbcf61
Paulo <pauloh16.pdr@gmail.com>
2021-03-13 20:19:28 -0300

    Ajustar buscas de turmas

--- server/api/turmas/get_turmas.js
+++ server/api/turmas/get_turmas.js
@@ -1,12 +1,26 @@
 const dbCon = require('../../db.js');
 
+const filtrarOcorrencias = (lista, id) => {
+    const listaFiltrada = lista.filter(({ id_aluno }) => id_aluno === id);
+    const listaFormatada = listaFiltrada.map(({ data_criacao, titulo, conteudo, criado_por }) => ({
+        data_criacao,
+        titulo,
+        conteudo,
+        criado_por
+    }));
+
+    return listaFormatada;
+};
+
 const filtrarAlunos = (lista, id) => {
     const listaFiltrada = lista.filter(({ id_turma }) => id_turma === id);
-    const listaFormatada = listaFiltrada.map(({ nome, email, telefone, data_nascimento }) => ({
-        nome,
-        email,
-        telefone,
-        data_nascimento: data_nascimento.toISOString().split('T')[0],
+    const listaFormatada = listaFiltrada.map((item) => ({
+        nome: item.nome,
+        email: item.email,
+        telefone: item.telefone,
+        data_nascimento: item.data_nascimento,
+        foto: item.foto,
+        ocorrencias: item.ocorrencias
     }));
 
     return listaFormatada;
@@ -43,15 +57,33 @@ module.exports = (req, res) => {
     
                     return;
                 }
-                
-                const dados = turmas.map((turma) => ({
-                    ...turma,
-                    formado: turma.formado === 1,
-                    alunos: filtrarAlunos(alunos, turma.id)
-                }));
 
-                console.log(`GET: Itens buscados ${dados.length}`);
-                res.status(200).send(dados);
+                dbCon.query('SELECT * FROM ocorrencias', (erroOcorrencias, ocorrencias) => {
+                    if (erroOcorrencias) {
+                        console.log(erroOcorrencias.sqlMessage);
+                        res.status(502).send({
+                            status: 'Falha',
+                            messagem: 'Erro ao buscar por dados na tabela ocorrencias.'
+                        });
+        
+                        return;
+                    }
+
+                    const dadosAlunos = alunos.map((aluno) => ({
+                        ...aluno,
+                        data_nascimento: aluno.data_nascimento.toISOString().split('T')[0],
+                        ocorrencias: filtrarOcorrencias(ocorrencias, aluno.id)
+                    }));
+                    
+                    const dadosTurmas = turmas.map((turma) => ({
+                        ...turma,
+                        formado: turma.formado === 1,
+                        alunos: filtrarAlunos(dadosAlunos, turma.id)
+                    }));
+    
+                    console.log(`GET: Itens buscados ${dadosTurmas.length}`);
+                    res.status(200).send(dadosTurmas);
+                });
             });
         });
     } catch ({ message }) {
