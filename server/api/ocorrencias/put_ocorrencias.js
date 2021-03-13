7b0d38d63970182a4197134776368ba9bda16f0e
Paulo <pauloh16.pdr@gmail.com>
2021-03-13 03:59:36 -0300

    Criar rota para atualizar dados de ocorrencias

--- server/api/ocorrencias/put_ocorrencias.js
+++ server/api/ocorrencias/put_ocorrencias.js
@@ -3,31 +3,30 @@ const dbCon = require('../../db.js');
 module.exports = (req, res) => {
     try {
         const { titulo, conteudo, criado_por, id_aluno } = req.body;
+        const { id } = req.params;
 
-        if (!titulo || !conteudo || !criado_por || !id_aluno ) {
+        if (!titulo || !conteudo || !criado_por || !id_aluno || !id) {
             const erro = JSON.stringify({ cod: 400, mensagem: 'Dados incompletos!' });
             throw new Error(erro);
         }
 
-        const data = new Date().toISOString().split('T')[0];
-
         const consulta = (
-            `INSERT INTO ocorrencias (id, titulo, conteudo, data_criacao, criado_por, id_aluno) VALUES
-            (null, '${titulo}', '${conteudo}', '${data}', '${criado_por}', ${id_aluno})`
+            `UPDATE ocorrencias SET titulo = '${titulo}', conteudo = '${conteudo}', criado_por = '${criado_por}', 
+            id_aluno = '${id_aluno}' WHERE id = ${id}`
         );
     
-        dbCon.query(consulta, (erroDB, resDB) => {
+        dbCon.query(consulta, (erroDB) => {
             if (erroDB) {
                 const erro = JSON.stringify({
                     cod: 502,
-                    mensagem: 'Erro ao inserir dados na tabela ocorrencias!'
+                    mensagem: 'Erro ao atualizar dados na tabela ocorrencias!'
                 });
 
                 throw new Error(erro);
             }
     
-            console.log(`POST: Itens adicionandos 1\nID: ${resDB.insertId}`);
-            res.status(201).send({ status: 'Sucesso', mensagem: 'Dados adicionados com sucesso!' });
+            console.log(`PUT: Itens atualizados 1\nID: ${id}`);
+            res.status(201).send({ status: 'Sucesso', mensagem: 'Dados atualizados com sucesso!' });
         });
     } catch ({ message }) {
         const { cod, mensagem } = JSON.parse(message);
--- server/api/ocorrencias/rotasOcorrencias.js
+++ server/api/ocorrencias/rotasOcorrencias.js
@@ -1,6 +1,7 @@
 // Arquivos de rotas.
 const getOcorrencias = require('./get_ocorrencias.js');
 const postOcorrencias = require('./post_ocorrencias.js');
+const putOcorrencias = require('./put_ocorrencias.js');
 
 module.exports = (app) => {
     // GET
@@ -8,4 +9,6 @@ module.exports = (app) => {
     app.get('/ocorrencias/:id', getOcorrencias);
     // POST
     app.post('/ocorrencias', postOcorrencias);
+    // PUT
+    app.put('/ocorrencias/:id', putOcorrencias);
 };
