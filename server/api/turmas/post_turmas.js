b901a855477f5342ea91a3e6def6ea9282d91c95
Paulo <pauloh16.pdr@gmail.com>
2021-03-13 06:33:51 -0300

    Criar rota para adicionar turma

---
+++ server/api/turmas/post_turmas.js
@@ -0,0 +1,35 @@
+const dbCon = require('../../db.js');
+
+module.exports = (req, res) => {
+    try {
+        const { nome, id_curso, formado } = req.body;
+
+        if (!nome || !id_curso || (formado !== 0 && formado !== 1)) {
+            const erro = JSON.stringify({ cod: 400, mensagem: 'Dados incompletos!' });
+            throw new Error(erro);
+        }
+
+        const consulta = (
+            `INSERT INTO turmas (id, nome, id_curso, formado) 
+            VALUES (null, '${nome}', '${id_curso}', ${formado})`
+        );
+
+        dbCon.query(consulta, (erroDB, resDB) => {
+            if (erroDB) {
+                console.log(erroDB.sqlMessage);
+                res.status(502).send({
+                    status: 'Falha',
+                    messagem: 'Erro ao adicionar dados na tabela turmas.'
+                });
+
+                return;
+            }
+    
+            console.log(`POST: Itens adicionandos 1\nID: ${resDB.insertId}`);
+            res.status(201).send({ status: 'Sucesso', mensagem: 'Dados adicionados com sucesso!' });
+        });
+    } catch ({ message }) {
+        const { cod, mensagem } = JSON.parse(message);
+        res.status(cod).send({ status: 'Falha', mensagem });
+    }
+};
--- server/api/turmas/rotasTurmas.js
+++ server/api/turmas/rotasTurmas.js
@@ -1,8 +1,11 @@
 // Arquivos de rotas.
 const getTurmas = require('./get_turmas.js');
+const postTurmas = require('./post_turmas.js');
 
 module.exports = (app) => {
     // GET
     app.get('/turmas', getTurmas);
     app.get('/turmas/:id', getTurmas);
+    // POST
+    app.post('/turmas', postTurmas);
 };
