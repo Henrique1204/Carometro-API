5d0995c6e325aa319f91284b37d3a2ec8cab75a7
Paulo <pauloh16.pdr@gmail.com>
2021-03-14 20:22:02 -0300

    Remover consoles de teste

--- server/corsCofnig.js
+++ server/corsCofnig.js
@@ -2,7 +2,6 @@ const cors = require('cors');
 
 module.exports = (app) => {
     app.use((req, res, next) => {
-        console.log("Opa");
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
